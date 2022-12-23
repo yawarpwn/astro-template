05fdc47a90f003de2e753c11d9c4aa3bbb1039f7
yawarpwn <john.kazz@gmail.com>
2022-12-23 10:11:11 -0500

    add prect tailwind image in astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,16 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
+import preact from "@astrojs/preact";
+
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://astro.build'
+  site: 'https://astro.build',
+  integrations: [tailwind(), image(), preact()]
 });
---
+++ src/data/site.ts
@@ -0,0 +1,8 @@
+const site = {
+  title: 'Astro',
+  description: 'Default description norma',
+  image: 'default image',
+  twitterHandler: 'astrotwitter'
+}
+
+export default site
