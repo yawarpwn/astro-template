edde7ade7673e2f2d9ff8306dbad26cebf2c7d2f
yawarpwn <john.kazz@gmail.com>
2022-12-26 10:11:53 -0500

    header ...

--- src/data/site.ts
+++ src/data/site.ts
@@ -2,8 +2,39 @@ import social from '../assets/social.png'
 const site = {
   title: 'Astro',
   description: 'Default description norma',
-  image:social, 
-  twitterHandle: 'astrotwitter'
+  image: social,
+  twitterHandle: 'astrotwitter',
+  socialLinks: [
+    {
+      href: 'https://github.com/withastro/astro',
+      me: 'https://github.com/withastro/',
+      text: "Go to Astro's GitHub repo",
+      pack: 'bi',
+      name: 'github'
+    },
+    {
+      href: '/chat',
+      text: 'Join the Astro community on Discord',
+      pack: 'cib',
+      name: 'discord'
+    },
+    {
+      href: 'https://twitter.com/astrodotbuild',
+      me: 'https://twitter.com/astrodotbuild',
+      text: 'Follow Astro on Twitter',
+      pack: 'bi',
+      name: 'twitter'
+    },
+    {
+      href: 'https://m.webtoo.ls/@astro',
+      me: 'https://m.webtoo.ls/@astro',
+      text: 'Follow Astro on Mastodon',
+      pack: 'bi',
+      name: 'mastodon',
+      footerOnly: true
+    }
+  ]
+
 }
 
 export default site
--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
 export type Link = {
-  title: string
+  text: string
   href: string
 }
